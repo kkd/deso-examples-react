df6dfc39d886f5a5587a14d106fdc6b88fadbba3
Takeshi Kakeda <takeshi@giantech.jp>
2023-03-08 15:56:39 +0900

    deso-protocol を使うように修正

--- src/routes/get-posts.jsx
+++ src/routes/get-posts.jsx
@@ -1,4 +1,5 @@
-import { identity } from "@deso-core/identity";
+
+import { identity, getPostsStateless, getPostsForUser } from "deso-protocol";
 import { useContext } from "react";
 import { useState } from "react";
 import { UserContext } from "../contexts";
@@ -16,38 +17,34 @@ export const GetPosts = () => {
     );
   }
 
+  console.log(user.PublicKeyBase58Check);
+
   const createFetch = () => {
-    fetch("https://node.deso.org/api/v0/get-posts-for-public-key", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        PublicKeyBase58Check: user.PublicKeyBase58Check,
-        ReaderPublicKeyBase58Check: user.PublicKeyBase58Check,
-        NumToFetch: 10,
-      }),
-    }).then((res) => res.json()
-    ).then((data) => {
-      console.log(data);
-      setPosts(data.Posts.map((post) => 
-      <div>
-        {post.Body} 
+    getPostsForUser({
+      PublicKeyBase58Check: user.PublicKeyBase58Check,
+      NumToFetch: 10,
+    }).then((res) => {
+      console.log(res);
+      setPosts(res.Posts.map((post) => 
         <div>
-          ❤️{post.LikeCount} 💎{post.DiamondCount} 🔁{post.RepostCount} “”{post.QuoteRepostCount}
-        </div>
-        <div>
-          {
-            post.ImageURLs ? (
-              post.ImageURLs.map((url) => 
-                <div>
-                  <img src={url} alt={url}/><br />
-                </div>
-              )) : (
-                undefined
-              )
-          }
-        </div>
-      </div>))
-    });
+          {post.Body} 
+          <div>
+            ❤️{post.LikeCount} 💎{post.DiamondCount} 🔁{post.RepostCount} “”{post.QuoteRepostCount}
+          </div>
+          <div>
+            {
+              post.ImageURLs ? (
+                post.ImageURLs.map((url) => 
+                  <div>
+                    <img src={url} alt={url}/><br />
+                  </div>
+                )) : (
+                  undefined
+                )
+            }
+          </div>
+        </div>));
+      });
   }
 
   return (
